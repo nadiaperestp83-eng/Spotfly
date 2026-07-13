ef7e02ac2faf5a1a0da2e6347d1f9fcad34dd23e
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:56:10 -0300

    Update search_state.dart

--- lib/features/search/state/search_state.dart
+++ lib/features/search/state/search_state.dart
@@ -1,4 +1,5 @@
 import 'package:meta/meta.dart';
+
 import '../../../core/models/track.dart';
 
 @immutable
@@ -7,9 +8,17 @@ class SearchState {
   final bool isLoading;
   final String? query;
 
-  const SearchState({this.results = const [], this.isLoading = false, this.query});
+  const SearchState({
+    this.results = const [],
+    this.isLoading = false,
+    this.query,
+  });
 
-  SearchState copyWith({List<Track>? results, bool? isLoading, String? query}) {
+  SearchState copyWith({
+    List<Track>? results,
+    bool? isLoading,
+    String? query,
+  }) {
     return SearchState(
       results: results ?? this.results,
       isLoading: isLoading ?? this.isLoading,
