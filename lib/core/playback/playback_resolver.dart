a3d6757e5d81dc69055a0fbcd2e8333717d59d6d
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:50:34 -0300

    Update playback_resolver.dart

--- lib/core/playback/playback_resolver.dart
+++ lib/core/playback/playback_resolver.dart
@@ -1,5 +1,19 @@
+import '../../features/search/data/i_music_source.dart';
 import '../models/track.dart';
+import 'i_playback_resolver.dart';
 
-abstract class IPlaybackResolver {
-  Future<String> resolve(Track track);
+class PlaybackResolver implements IPlaybackResolver {
+  final Map<String, IMusicSource> _sourcesById;
+
+  PlaybackResolver(List<IMusicSource> sources)
+      : _sourcesById = {for (final s in sources) s.sourceId: s};
+
+  @override
+  Future<String> resolve(Track track) async {
+    final source = _sourcesById[track.sourceId];
+    if (source == null) {
+      throw StateError('Fonte "${track.sourceId}" não registrada.');
+    }
+    return source.resolveStreamUrl(track);
+  }
 }
