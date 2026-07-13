a840e75086f88f9c7c5ad4abd83bb2ee7a8b417c
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:27:33 -0300

    Create youtube_source.dart

---
+++ lib/features/search/data/sources/youtube_source.dart
@@ -0,0 +1,35 @@
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import '../../../../core/models/track.dart';
+import '../i_music_source.dart';
+
+class YoutubeSource implements IMusicSource {
+  final YoutubeExplode _yt = YoutubeExplode();
+
+  @override
+  String get sourceId => 'youtube';
+
+  @override
+  Future<List<Track>> search(String query) async {
+    final results = await _yt.search.search(query);
+    return results.whereType<Video>().map((v) {
+      return Track(
+        id: '${sourceId}_${v.id.value}',
+        title: v.title,
+        artist: v.author,
+        artworkUrl: v.thumbnails.highResUrl,
+        duration: v.duration,
+        sourceId: sourceId,
+        sourceTrackId: v.id.value,
+        bitrateKbps: 160, // estimativa até resolver o manifest real
+      );
+    }).toList();
+  }
+
+  @override
+  Future<String> resolveStreamUrl(Track track) async {
+    final manifest =
+        await _yt.videos.streamsClient.getManifest(track.sourceTrackId);
+    final audio = manifest.audioOnly.withHighestBitrate();
+    return audio.url.toString();
+  }
+}
