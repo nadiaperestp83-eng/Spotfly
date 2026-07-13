087eff54a25c2287845ea2200334e1c4eadc3c05
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:29:27 -0300

    Create search_state.dart

---
+++ lib/features/search/state/search_state.dart
@@ -0,0 +1,19 @@
+import 'package:meta/meta.dart';
+import '../../../core/models/track.dart';
+
+@immutable
+class SearchState {
+  final List<Track> results;
+  final bool isLoading;
+  final String? query;
+
+  const SearchState({this.results = const [], this.isLoading = false, this.query});
+
+  SearchState copyWith({List<Track>? results, bool? isLoading, String? query}) {
+    return SearchState(
+      results: results ?? this.results,
+      isLoading: isLoading ?? this.isLoading,
+      query: query ?? this.query,
+    );
+  }
+}
