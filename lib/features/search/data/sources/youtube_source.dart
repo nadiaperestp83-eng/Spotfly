97ad7e37ae720723bc6d02672a3376696e2bd3ff
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:52:53 -0300

    Update youtube_source.dart

--- lib/features/search/data/sources/youtube_source.dart
+++ lib/features/search/data/sources/youtube_source.dart
@@ -1,4 +1,5 @@
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
 import '../../../../core/models/track.dart';
 import '../i_music_source.dart';
 
@@ -10,19 +11,23 @@ class YoutubeSource implements IMusicSource {
 
   @override
   Future<List<Track>> search(String query) async {
-    final results = await _yt.search.search(query);
-    return results.whereType<Video>().map((v) {
-      return Track(
-        id: '${sourceId}_${v.id.value}',
-        title: v.title,
-        artist: v.author,
-        artworkUrl: v.thumbnails.highResUrl,
-        duration: v.duration,
-        sourceId: sourceId,
-        sourceTrackId: v.id.value,
-        bitrateKbps: 160, // estimativa até resolver o manifest real
-      );
-    }).toList();
+    try {
+      final results = await _yt.search.search(query);
+      return results.whereType<Video>().map((v) {
+        return Track(
+          id: '${sourceId}_${v.id.value}',
+          title: v.title,
+          artist: v.author,
+          artworkUrl: v.thumbnails.highResUrl,
+          duration: v.duration,
+          sourceId: sourceId,
+          sourceTrackId: v.id.value,
+          bitrateKbps: 160,
+        );
+      }).toList();
+    } catch (_) {
+      return [];
+    }
   }
 
   @override
