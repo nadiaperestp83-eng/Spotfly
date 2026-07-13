20a300507ba30020d232f912c259b0b01cb1e0ab
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:58:27 -0300

    Create providers.dart

---
+++ lib/core/providers/providers.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../features/search/data/i_music_source.dart';
+import '../../features/search/data/sources/jamendo_source.dart';
+import '../../features/search/data/sources/piped_source.dart';
+import '../../features/search/data/sources/youtube_source.dart';
+import '../../features/search/domain/search_coordinator.dart';
+import '../audio/i_audio_player_service.dart';
+import '../audio/just_audio_player_service.dart';
+import '../playback/i_playback_resolver.dart';
+import '../playback/playback_resolver.dart';
+
+final audioPlayerServiceProvider = Provider<IAudioPlayerService>((ref) {
+  final service = JustAudioPlayerService();
+  ref.onDispose(service.dispose);
+  return service;
+});
+
+final musicSourcesProvider = Provider<List<IMusicSource>>((ref) {
+  return [
+    YoutubeSource(),
+    JamendoSource(
+      clientId: const String.fromEnvironment('JAMENDO_CLIENT_ID'),
+    ),
+    PipedSource(),
+  ];
+});
+
+final playbackResolverProvider = Provider<IPlaybackResolver>((ref) {
+  return PlaybackResolver(ref.watch(musicSourcesProvider));
+});
+
+final searchCoordinatorProvider = Provider<SearchCoordinator>((ref) {
+  return SearchCoordinator(ref.watch(musicSourcesProvider));
+});
