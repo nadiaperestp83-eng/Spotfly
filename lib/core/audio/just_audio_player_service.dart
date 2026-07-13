6cdc82d787d63b98db04a2d23b77e49ea738d7d5
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:23:38 -0300

    Create just_audio_player_service.dart

---
+++ lib/core/audio/just_audio_player_service.dart
@@ -0,0 +1,39 @@
+import 'package:just_audio/just_audio.dart';
+import 'i_audio_player_service.dart';
+
+class JustAudioPlayerService implements IAudioPlayerService {
+  final AudioPlayer _player = AudioPlayer();
+
+  @override
+  Stream<Duration> get positionStream => _player.positionStream;
+
+  @override
+  Stream<Duration?> get durationStream => _player.durationStream;
+
+  @override
+  Stream<bool> get playingStream => _player.playingStream;
+
+  @override
+  Future<void> play(String url) async {
+    await _player.setUrl(url);
+    await _player.play();
+  }
+
+  @override
+  Future<void> pause() => _player.pause();
+
+  @override
+  Future<void> resume() => _player.play();
+
+  @override
+  Future<void> stop() => _player.stop();
+
+  @override
+  Future<void> seek(Duration position) => _player.seek(position);
+
+  @override
+  Future<void> setVolume(double volume) => _player.setVolume(volume);
+
+  @override
+  Future<void> dispose() => _player.dispose();
+}
