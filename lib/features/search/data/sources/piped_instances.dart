b48bd57045ddc43d8f41da52d7ec59336c8a270b
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-13 16:54:03 -0300

    Create piped_instances.dart

---
+++ lib/features/search/data/sources/piped_instances.dart
@@ -0,0 +1,9 @@
+const List<String> pipedInstances = [
+  'https://pipedapi.kavin.rocks',
+  'https://pipedapi-libre.kavin.rocks',
+  'https://pipedapi.leptons.xyz',
+  'https://piped-api.privacy.com.de',
+  'https://pipedapi.adminforge.de',
+  'https://api.piped.yt',
+  'https://pipedapi.drgns.space',
+];
