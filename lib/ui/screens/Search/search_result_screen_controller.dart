7b17faea2cd72399ea786be4bc4a6d4e4f3e5951
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-14 04:12:48 -0300

    Update search_result_screen_controller.dart

--- lib/ui/screens/Search/search_result_screen_controller.dart
+++ lib/ui/screens/Search/search_result_screen_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:harmonymusic/ui/screens/Settings/settings_screen_controller.dart';
 
