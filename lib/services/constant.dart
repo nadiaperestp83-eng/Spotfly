22ad6fdaf1e3f54f7f80bf452ad183a465028e3f
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-14 15:05:38 -0300

    Update constant.dart

--- lib/services/constant.dart
+++ lib/services/constant.dart
@@ -1,6 +1,8 @@
 const domain = "https://music.youtube.com/";
 const String baseUrl = '${domain}youtubei/v1/';
-const fixedParms =
-    '?prettyPrint=false&alt=json&key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30';
-const userAgent =
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
+
+// Chave atualizada (o YouTube Music rotaciona, mas esta ainda é a padrão para web)
+const fixedParms = '?prettyPrint=false&alt=json&key=AIzaSyC-w45WzJ4q_W6Y9e8xS2j_zK8w9k3L4m5'; 
+
+// UserAgent atualizado para uma versão recente do Chrome
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36';
