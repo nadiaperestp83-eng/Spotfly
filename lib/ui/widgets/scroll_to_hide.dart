ac5555cfe47c9bbceade12951b32e7247fa5f356
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-16 19:46:44 -0300

    Update scroll_to_hide.dart

--- lib/ui/widgets/scroll_to_hide.dart
+++ lib/ui/widgets/scroll_to_hide.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'bottom_nav_bar.dart';
+
 class ScrollToHideWidget extends StatelessWidget {
   const ScrollToHideWidget(
       {super.key, required this.isVisible, required this.child});
@@ -9,9 +11,19 @@ class ScrollToHideWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // IMPORTANTE: essa altura precisa bater exatamente com a soma das
+    // margens + altura da pílula em bottom_nav_bar.dart
+    // (top + pill + bottom + safe-area). Se for maior, o Container(height:
+    // kFloatingNavBarHeight) de dentro do BottomNavBar recebe constraints
+    // tight do AnimatedContainer e é esticado além de 52dp, perdendo o
+    // visual compacto e refinado da pílula.
+    final floatingNavBarTotalHeight = kFloatingNavBarTopMargin +
+        kFloatingNavBarHeight +
+        kFloatingNavBarBottomMargin +
+        Get.mediaQuery.viewPadding.bottom;
     return AnimatedContainer(
       duration: const Duration(milliseconds: 200),
-      height: isVisible ? 80.0 + Get.mediaQuery.viewPadding.bottom : 0.0,
+      height: isVisible ? floatingNavBarTotalHeight : 0.0,
       child: child,
     );
   }
