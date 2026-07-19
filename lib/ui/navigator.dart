ed5775315233e73c55d584466943094090871935
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-19 14:49:09 -0300

    Update navigator.dart

--- lib/ui/navigator.dart
+++ lib/ui/navigator.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:harmonymusic/models/album.dart';
 import 'package:harmonymusic/models/artist.dart';
+import 'package:harmonymusic/ui/screens/Home/home_screen.dart';
+
 
 import 'package:harmonymusic/ui/screens/Artists/artist_screen.dart';
 import 'package:harmonymusic/ui/screens/Home/home_screen.dart';
